372cd07e734c2414361993e7aa4f1735812af498
juanpchica <juanpchica@hotmail.com>
2021-05-12 09:25:18 -0500

    Sequelize and mysql configure with initial users model

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+const { sequelize } = require("./models");
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
+
+  //Connect to db with sequelize
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Db connected!!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
