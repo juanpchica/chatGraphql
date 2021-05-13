5e8a564a39e646af3532c079b49a96d3bd31ef8c
juanpchica <juanpchica@hotmail.com>
2021-05-13 10:45:29 -0500

    Router added with login and home page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import { Container } from "react-bootstrap";
 import ApolloProvide from "./ApolloProvider";
-import "./App.scss";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
+
+import "./App.scss";
 
 function App() {
   return (
     <ApolloProvide>
       <Container className='pt-5'>
-        <Register />
+        <BrowserRouter>
+          <Container className='pt-5'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/register' component={Register} />
+              <Route path='/login' component={Login} />
+            </Switch>
+          </Container>
+        </BrowserRouter>
       </Container>
     </ApolloProvide>
   );
