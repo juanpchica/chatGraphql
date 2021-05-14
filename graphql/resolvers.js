56fc0630a5fe4fccbcd511c429737dd92ad094f3
juanpchica <juanpchica@hotmail.com>
2021-05-14 09:39:40 -0500

    Message mutation added in resolvers with some extra validations

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { Message, User } = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { Op } = require("sequelize");
@@ -119,6 +119,35 @@ module.exports = {
           error.errors.forEach((e) => (errors[e.path] = e.message));
         }
         throw new UserInputError("Input Error", { errors });
+      }
+    },
+    sendMessage: async (_, { to, content }, { user }) => {
+      if (!user) throw new AuthenticationError("Unauthenticated");
+
+      try {
+        //Validate if content message is not empty
+        if (content.trim() === "")
+          throw new UserInputError("Content cannot be empty");
+
+        //Validate if recipient exist
+        const recipient = await User.findOne({ where: { username: to } });
+        if (!recipient) {
+          throw new UserInputError("User not found!!");
+        } else if (recipient.username === user.username) {
+          throw new UserInputError("You cannot send messages to yourself!");
+        }
+
+        //Everything is ok then add new message
+        const message = await Message.create({
+          to,
+          from: user.username,
+          content,
+        });
+
+        return message;
+      } catch (error) {
+        console.log(error);
+        throw error;
       }
     },
   },
