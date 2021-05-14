6e7f88906d01f0fe45906a5581a295117155ce66
juanpchica <juanpchica@hotmail.com>
2021-05-14 08:37:41 -0500

    Context for authentication added

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,21 +6,11 @@ const { Op } = require("sequelize");
 const { UserInputError, AuthenticationError } = require("apollo-server-errors");
 module.exports = {
   Query: {
-    getUsers: async (_, __, context) => {
+    getUsers: async (_, __, { user }) => {
       try {
-        let user;
+        if (!user) throw new AuthenticationError("Unathenticated");
 
         //Context checking
-        if (context.req && context.req.headers.authorization) {
-          const token = context.req.headers.authorization.split("Bearer ")[1];
-
-          jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
-            if (err) {
-              throw new AuthenticationError("Unauthenticated");
-            }
-            user = decodedToken;
-          });
-        }
 
         //Get all users but not the one who is logged in
         const users = await User.findAll({
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { sequelize } = require("./models");
+const contextMiddleware = require("./util/contextMiddleware");
 
 require("dotenv").config();
 
-const server = new ApolloServer({ typeDefs, resolvers, context: (ctx) => ctx });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: contextMiddleware,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
