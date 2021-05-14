324bf781ee5a0c8b4e1de25505870962c636b023
juanpchica <juanpchica@hotmail.com>
2021-05-14 15:03:26 -0500

    Decode token and save it in localstorage as a state

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,8 +1,24 @@
 import { createContext, useReducer, useContext } from "react";
+import jwtDecode from "jwt-decode";
 
 //Create context to use the reduce in all app
 const AuthStateContext = createContext();
 const AuthDispatchContext = createContext();
+
+let user = null;
+
+//Validate if token exist and get user data from token
+const token = localStorage.getItem("token");
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (new Date() > new Date(decodedToken.exp * 1000)) {
+    localStorage.deleteItem("token");
+  } else {
+    user = decodedToken;
+  }
+} else {
+  console.log("Not token found");
+}
 
 const authReduce = (state, action) => {
   switch (action.type) {
@@ -13,7 +29,9 @@ const authReduce = (state, action) => {
         user: action.payload,
       };
     case "LOGOUT":
+      localStorage.deleteItem("token");
       return {
+        ...state,
         user: null,
       };
     default:
@@ -22,7 +40,7 @@ const authReduce = (state, action) => {
 };
 
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReduce, { user: null });
+  const [state, dispatch] = useReducer(authReduce, { user });
   return (
     <AuthDispatchContext.Provider value={dispatch}>
       <AuthStateContext.Provider value={state}>
