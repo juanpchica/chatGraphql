57e4f513466a7cc7224babe8585af3e0dbfa32c1
juanpchica <juanpchica@hotmail.com>
2021-05-20 09:22:35 -0500

    Model reactions changed, some columns added and db migrated

--- models/reaction.js
+++ models/reaction.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Reaction extends Model {
     /**
@@ -9,16 +7,36 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ User, Message }) {
       // define association here
+      this.belongsTo(Message);
+      this.belongsTo(User);
     }
-  };
-  Reaction.init({
-    content: DataTypes.STRING,
-    uuid: DataTypes.UUID
-  }, {
-    sequelize,
-    modelName: 'Reaction',
-  });
+  }
+  Reaction.init(
+    {
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      messageId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Reaction",
+      tableName: "reactions",
+    }
+  );
   return Reaction;
 };
