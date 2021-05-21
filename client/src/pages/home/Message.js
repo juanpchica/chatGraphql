d0dbbfb5e92721f46c7fb3d47f99350827b6429a
juanpchica <juanpchica@hotmail.com>
2021-05-21 09:41:10 -0500

    Message component reactions added and connected with reaction mutation in server

--- client/src/pages/home/Message.js
+++ client/src/pages/home/Message.js
@@ -1,29 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import moment from "moment";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { OverlayTrigger, Tooltip, Popover, Button } from "react-bootstrap";
 
 import { useAuthState } from "../../context/auth";
+import { gql, useMutation } from "@apollo/client";
+
+const reactions = ["❤️", "😆", "😯", "😢", "😡", "👍", "👎"];
+
+const REACT_TO_MESSAGE = gql`
+  mutation reactToMessage($uuid: String!, $content: String!) {
+    reactToMessage(uuid: $uuid, content: $content) {
+      uuid
+    }
+  }
+`;
 
 const Message = ({ message }) => {
   const { user } = useAuthState();
   const sent = message.from === user.username;
   const received = !sent;
-  return (
+
+  const [showPopover, setShowPopover] = useState(false);
+  const reactionIcons = [...new Set(message.reactions.map((r) => r.content))];
+
+  const [reactToMessage] = useMutation(REACT_TO_MESSAGE, {
+    onError: (err) => console.log(err),
+    onCompleted: (data) => setShowPopover(false),
+  });
+
+  const react = (reaction) => {
+    reactToMessage({ variables: { uuid: message.uuid, content: reaction } });
+  };
+
+  const reactButton = (
     <OverlayTrigger
-      placement={sent ? "right" : "left"}
-      overlay={
-        <Tooltip>
-          {moment(message.createdAt).format("MMMM DD, YYYY @ h:mm a")}
-        </Tooltip>
-      }
+      trigger='click'
+      placement='top'
+      show={showPopover}
+      onToggle={setShowPopover}
       transition={false}
+      rootClose
+      overlay={
+        <Popover className='rounded-pill'>
+          <Popover.Content className='px-0 py-1 d-flex align-items-center react-button-popover'>
+            {reactions.map((reaction) => (
+              <Button
+                variant='link'
+                className='react-icon-button'
+                key={reaction}
+                onClick={() => react(reaction)}
+              >
+                {reaction}
+              </Button>
+            ))}
+          </Popover.Content>
+        </Popover>
+      }
     >
-      <div
-        className={classNames("d-flex my-3", {
-          "ml-auto": sent,
-          "mr-auto": received,
-        })}
+      <Button variant='link' className='px-2'>
+        <i className='far fa-smile'></i>
+      </Button>
+    </OverlayTrigger>
+  );
+
+  return (
+    <div
+      className={classNames("d-flex my-3", {
+        "ml-auto": sent,
+        "mr-auto": received,
+      })}
+    >
+      {sent && reactButton}
+      <OverlayTrigger
+        placement={sent ? "right" : "left"}
+        overlay={
+          <Tooltip>
+            {moment(message.createdAt).format("MMMM DD, YYYY @ h:mm a")}
+          </Tooltip>
+        }
+        transition={false}
       >
         <div
           className={classNames("py-2 px-3 rounded-pill", {
@@ -31,12 +87,18 @@ const Message = ({ message }) => {
             "bg-secondary": received,
           })}
         >
+          {message.reactions.length > 0 && (
+            <div className='p-1 reactions-div bg-secondary rounded-pill'>
+              {reactionIcons} {message.reactions.length}
+            </div>
+          )}
           <p className={classNames({ "text-white": sent })} key={message.uuid}>
             {message.content}
           </p>
         </div>
-      </div>
-    </OverlayTrigger>
+      </OverlayTrigger>
+      {received && reactButton}
+    </div>
   );
 };
 
